6022f58dd79c81d20ad4507c96e0200d77cb25bf
Maitland Marshall <maitlandmarshallapps@gmail.com>
2018-08-03 14:46:49 +1000

    initial commit

---
+++ XPay/Platform/iOS/ApplePay.cs
@@ -0,0 +1,84 @@
+﻿using Foundation;
+using PassKit;
+using System;
+using System.Threading.Tasks;
+using UIKit;
+using Xamarin.Forms;
+using XPay.Platform.iOS;
+using XPay.Shared;
+
+[assembly: Dependency(typeof(ApplePay))]
+namespace XPay.Platform.iOS
+{
+    public class ApplePay : PKPaymentAuthorizationViewControllerDelegate, IXPay
+    {
+        PKPaymentAuthorizationViewController paymentController;
+
+        public async Task<bool> Show(XPayRequest request)
+        {
+            NSString[] paymentNetworks = new NSString[] { PKPaymentNetwork.MasterCard, PKPaymentNetwork.Visa };
+
+            if (!PKPaymentAuthorizationViewController.CanMakePayments || !PKPaymentAuthorizationViewController.CanMakePaymentsUsingNetworks(paymentNetworks))
+            {
+                throw new NotSupportedException();
+            }
+
+            PKPaymentSummaryItem
+                paymentLine = PKPaymentSummaryItem.Create("Tap and go payment", new NSDecimalNumber("10")),
+                feeLine = PKPaymentSummaryItem.Create("Fee", new NSDecimalNumber("0")),
+                totalLine = PKPaymentSummaryItem.Create("GitBit", paymentLine.Amount.Add(feeLine.Amount));
+
+            PKPaymentRequest paymentRequest = new PKPaymentRequest();
+            paymentRequest.PaymentSummaryItems = new PKPaymentSummaryItem[] { paymentLine, feeLine, totalLine };
+            paymentRequest.MerchantIdentifier = request.MerchantIdentifier;
+            paymentRequest.MerchantCapabilities = PKMerchantCapability.ThreeDS;
+            paymentRequest.CountryCode = request.CountryCode;
+            paymentRequest.CurrencyCode = request.CurrencyCode;
+            paymentRequest.SupportedNetworks = paymentNetworks;
+
+            this.paymentController = new PKPaymentAuthorizationViewController(paymentRequest);
+            this.paymentController.Delegate = this;
+
+            UIWindow window = UIApplication.SharedApplication.KeyWindow;
+            UIViewController vc = window.RootViewController;
+
+            while (vc.PresentedViewController != null)
+            {
+                vc = vc.PresentedViewController;
+            }
+
+            await vc.PresentViewControllerAsync(this.paymentController, true);
+            return true;
+        }
+
+        public async void Hide()
+        {
+            if (this.paymentController == null) return;
+
+            await this.paymentController.DismissViewControllerAsync(true);
+            this.paymentController = null;
+        }
+
+
+        #region PKPaymentAuthorizationControllerDelegate Implementation
+
+        public override void DidAuthorizePayment(PKPaymentAuthorizationViewController controller, PKPayment payment, Action<PKPaymentAuthorizationStatus> completion)
+        {
+            completion(PKPaymentAuthorizationStatus.Success);
+        }
+
+        public async override void PaymentAuthorizationViewControllerDidFinish(PKPaymentAuthorizationViewController controller)
+        {
+            await Task.Delay(100);
+            this.Hide();
+        }
+
+        public override void WillAuthorizePayment(PKPaymentAuthorizationViewController controller)
+        {
+
+        }
+
+
+        #endregion
+    }
+}
---
+++ XPay/Shared/XPay.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XPay.Shared
+{
+    public interface IXPay
+    {
+        Task<bool> Show(XPayRequest request);
+        void Hide();
+    }
+
+    public class XPay : IXPay
+    {
+        IXPay PaymentPlatform { get; set; }
+
+        public XPay()
+        {
+            if(Device.RuntimePlatform != Device.iOS && Device.RuntimePlatform != Device.Android)
+            {
+                throw new NotSupportedException();
+            }
+
+            this.PaymentPlatform = DependencyService.Get<IXPay>(DependencyFetchTarget.NewInstance);
+        }
+
+        public async Task<bool> Show(XPayRequest request)
+        {
+            request.Validate();
+            return await this.PaymentPlatform.Show(request);
+        }
+
+        public void Hide()
+        {
+            this.PaymentPlatform.Hide();
+        }
+    }
+
+    public class XPayRequest
+    {
+        public string MerchantIdentifier { get; set; }
+        public string CountryCode { get; set; }
+        public string CurrencyCode { get; set; }
+        public List<string> SupportedCountries { get; set; }
+
+        internal void Validate()
+        {
+            if (this.CountryCode.Length != 2)
+            {
+                throw new Exception("CountryCode must be 2 characters only (ISO 3166-1 alpha-2)");
+            }
+        }
+    }
+}
